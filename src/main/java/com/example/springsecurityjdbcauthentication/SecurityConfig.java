676fa19f52192882218d4ff806b89aeae39ded76
antara-walid <Antara.walid.27@gmail.com>
2022-08-23 15:41:53 +0100

    authentication using defaultSchema

--- src/main/java/com/example/springsecurityjdbcauthentication/SecurityConfig.java
+++ src/main/java/com/example/springsecurityjdbcauthentication/SecurityConfig.java
@@ -1,16 +1,40 @@
 package com.example.springsecurityjdbcauthentication;
 
 
+import lombok.Builder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
+import javax.sql.DataSource;
+
+@EnableWebSecurity @Builder
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication();
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()// here we let spring use it s default users and roles
+                .withUser(
+                       User.withUsername("user")
+                               .password("user")
+                               .roles("USER")
+                )
+
+                .withUser(
+                        User.withUsername("admin")
+                                .password("admin")
+                                .roles("ADMIN")
+                );
     }
 
     @Override
@@ -21,5 +45,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .and()
                 .formLogin();
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder()
+    {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
